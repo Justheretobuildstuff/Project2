6e40e185327cf87841254168ea2cc9489c74ab60
Josh <josh.daniels10@gmail.com>
2018-08-16 19:53:25 -0700

    update

--- analyzer/analyzeSentiment.js
+++ analyzer/analyzeSentiment.js
@@ -53,7 +53,7 @@ let get_sentiments = function (documents) {
 }
 
 let documents = { 'documents': [
-    { 'id': '1', 'language': 'en', 'text': 'I really enjoy the new XBox One S. It has a clean look, it has 4K/HDR resolution and it is affordable.' },
+    { 'id': '1', 'language': 'en', 'text': 'Douche Bag' },
     { 'id': '2', 'language': 'es', 'text': 'Este ha sido un dia terrible, llegué tarde al trabajo debido a un accidente automobilistico.' },
 ]};
 
