e21f255be2205d74e34433c1b67e8b5aa241fc16
DanielG <Danielmorrisg@gmail.com>
2018-08-21 20:30:16 -0700

    changed server to start working with handlebars

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,4 @@
 const testApiController = require("../controllers/apiController.js");
-const sentimentResponse = require("../analyzeSentiment.js");
-
-console.log(sentimentResponse.documents);
 
 module.exports = function(app) {
   app.get("/test", testApiController.index);
@@ -10,69 +7,22 @@ module.exports = function(app) {
   var db = require("../models");
 
   // GET route for getting all of the todos
-  app.get("/api/todos", function(req, res) {
+  app.get("/api/all", function(req, res) {
     // findAll returns all entries for a table when used with no options
-    db.Todo.findAll({}).then(function(dbTodo) {
+    db.Todo.findAll({}).then(function(result) {
       // We have access to the todos as an argument inside of the callback function
-      res.json(dbTodo);
+      res.json(result);
     });
   });
 
-  // POST route for saving a new todo
-  app.post("/api/todos", function(req, res) {
-    // create takes an argument of an object describing the item we want to
-    // insert into our table. In this case we just we pass in an object with a text
-    // and complete property (req.body)
-    db.Todo.create({
-      text: req.body.text,
-      complete: req.body.complete
-    })
-      .then(function(dbTodo) {
-        // We have access to the new todo as an argument inside of the callback function
-        res.json(dbTodo);
-      })
-      .catch(function(err) {
-        // Whenever a validation or flag fails, an error is thrown
-        // We can "catch" the error to prevent it from being "thrown", which could crash our node app
-        res.json(err);
-      });
-  });
-
-  // DELETE route for deleting todos. We can get the id of the todo to be deleted from
-  // req.params.id
-  app.delete("/api/todos/:id", function(req, res) {
-    // We just have to specify which todo we want to destroy with "where"
-    db.Todo.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbTodo) {
-      res.json(dbTodo);
+  app.post("/api/new", function(req, res) {
+    const text = req.body.text;
+    const category = req.body.category;
+    db.Item.create({
+      text: text,
+      category: category
+    }).then(function(result) {
+      res.json(result);
     });
-  });
-
-  // PUT route for updating todos. We can get the updated todo data from req.body
-  app.put("/api/todos", function(req, res) {
-    // Update takes in an object describing the properties we want to update, and
-    // we use where to describe which objects we want to update
-    db.Todo.update(
-      {
-        text: req.body.text,
-        complete: req.body.complete
-      },
-      {
-        where: {
-          id: req.body.id
-        }
-      }
-    )
-      .then(function(dbTodo) {
-        res.json(dbTodo);
-      })
-      .catch(function(err) {
-        // Whenever a validation or flag fails, an error is thrown
-        // We can "catch" the error to prevent it from being "thrown", which could crash our node app
-        res.json(err);
-      });
   });
 };
